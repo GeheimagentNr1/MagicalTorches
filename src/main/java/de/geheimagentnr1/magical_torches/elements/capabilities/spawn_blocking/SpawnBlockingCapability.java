703fc0b839ec22e2235e8e13155f3d1397354a2c
GeheimagentNr1 <GeheimeLache@hotmail.com>
2020-02-28 22:13:56 +0100

    NBT serialize und deserialize vorsorglich für neue Features outgesourced

--- src/main/java/de/geheimagentnr1/magical_torches/elements/capabilities/spawn_blocking/SpawnBlockingCapability.java
+++ src/main/java/de/geheimagentnr1/magical_torches/elements/capabilities/spawn_blocking/SpawnBlockingCapability.java
@@ -3,16 +3,14 @@ package de.geheimagentnr1.magical_torches.elements.capabilities.spawn_blocking;
 import de.geheimagentnr1.magical_torches.elements.capabilities.ModCapabilities;
 import de.geheimagentnr1.magical_torches.helpers.RadiusHelper;
 import de.geheimagentnr1.magical_torches.helpers.ResourceLocationBuilder;
+import de.geheimagentnr1.magical_torches.helpers.SpawnBlockerNBTHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
-import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.LazyOptional;
 
 import javax.annotation.Nonnull;
@@ -32,14 +30,6 @@ public class SpawnBlockingCapability implements ICapabilitySerializable<ListNBT>
 	private TreeSet<SpawnBlocker> spawnBlockers = new TreeSet<>( Comparator.comparing( SpawnBlocker::getPos ) );
 	
 	private final static TreeMap<ResourceLocation, ISpawnBlockFactory> SPAWN_BLOCKING_REGISTERY = new TreeMap<>();
-	
-	private final static String registryNameName = "registry_name";
-	
-	private final static String xName = "x";
-	
-	private final static String yName = "y";
-	
-	private final static String zName = "z";
 	
 	public static void registerSpawnBlocker( ResourceLocation _registry_name, ISpawnBlockFactory factory ) {
 		
@@ -71,44 +61,13 @@ public class SpawnBlockingCapability implements ICapabilitySerializable<ListNBT>
 	@Override
 	public ListNBT serializeNBT() {
 		
-		ListNBT nbt = new ListNBT();
-		
-		for( SpawnBlocker spawnBlocker : spawnBlockers ) {
-			BlockPos pos = spawnBlocker.getPos();
-			CompoundNBT compoundNBT = new CompoundNBT();
-			compoundNBT.putString( registryNameName, spawnBlocker.getRegistryName().toString() );
-			compoundNBT.putInt( xName, pos.getX() );
-			compoundNBT.putInt( yName, pos.getY() );
-			compoundNBT.putInt( zName, pos.getZ() );
-			nbt.add( compoundNBT );
-		}
-		return nbt;
+		return SpawnBlockerNBTHelper.serializeSpawnBlockers( spawnBlockers );
 	}
 	
 	@Override
 	public void deserializeNBT( ListNBT nbt ) {
 		
-		spawnBlockers = new TreeSet<>( Comparator.comparing( SpawnBlocker::getPos ) );
-		for( INBT inbt : nbt ) {
-			if( inbt instanceof CompoundNBT ) {
-				CompoundNBT compoundNBT = (CompoundNBT)inbt;
-				if( compoundNBT.contains( registryNameName, Constants.NBT.TAG_STRING ) ) {
-					String registry_name_string = compoundNBT.getString( registryNameName );
-					if( ResourceLocation.isResouceNameValid( registry_name_string ) &&
-						compoundNBT.contains( xName, Constants.NBT.TAG_INT ) &&
-						compoundNBT.contains( yName, Constants.NBT.TAG_INT ) &&
-						compoundNBT.contains( zName, Constants.NBT.TAG_INT ) ) {
-						ResourceLocation registry_name = new ResourceLocation( registry_name_string );
-						BlockPos pos = new BlockPos( new BlockPos( compoundNBT.getInt( xName ),
-							compoundNBT.getInt( yName ), compoundNBT.getInt( zName ) ) );
-						ISpawnBlockFactory factory = SPAWN_BLOCKING_REGISTERY.get( registry_name );
-						if( factory != null ) {
-							spawnBlockers.add( factory.buildSpawnBlocker( pos ) );
-						}
-					}
-				}
-			}
-		}
+		spawnBlockers = SpawnBlockerNBTHelper.deserializeSpawnBlockers( nbt, SPAWN_BLOCKING_REGISTERY );
 	}
 	
 	public void addSpawnBlocker( SpawnBlocker spawnBlocker ) {
---
+++ src/main/java/de/geheimagentnr1/magical_torches/helpers/SpawnBlockerNBTHelper.java
@@ -0,0 +1,70 @@
+package de.geheimagentnr1.magical_torches.helpers;
+
+import de.geheimagentnr1.magical_torches.elements.capabilities.spawn_blocking.ISpawnBlockFactory;
+import de.geheimagentnr1.magical_torches.elements.capabilities.spawn_blocking.SpawnBlocker;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.util.Constants;
+
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+public class SpawnBlockerNBTHelper {
+	
+	
+	private final static String registryNameName = "registry_name";
+	
+	private final static String xName = "x";
+	
+	private final static String yName = "y";
+	
+	private final static String zName = "z";
+	
+	public static ListNBT serializeSpawnBlockers( TreeSet<SpawnBlocker> spawnBlockers ) {
+		
+		ListNBT nbt = new ListNBT();
+		
+		for( SpawnBlocker spawnBlocker : spawnBlockers ) {
+			BlockPos pos = spawnBlocker.getPos();
+			CompoundNBT compoundNBT = new CompoundNBT();
+			compoundNBT.putString( registryNameName, spawnBlocker.getRegistryName().toString() );
+			compoundNBT.putInt( xName, pos.getX() );
+			compoundNBT.putInt( yName, pos.getY() );
+			compoundNBT.putInt( zName, pos.getZ() );
+			nbt.add( compoundNBT );
+		}
+		return nbt;
+	}
+	
+	public static TreeSet<SpawnBlocker> deserializeSpawnBlockers( ListNBT nbt,
+		TreeMap<ResourceLocation, ISpawnBlockFactory> SPAWN_BLOCKING_REGISTERY  ) {
+		
+		TreeSet<SpawnBlocker> spawnBlockers = new TreeSet<>( Comparator.comparing( SpawnBlocker::getPos ) );
+		for( INBT inbt : nbt ) {
+			if( inbt instanceof CompoundNBT ) {
+				CompoundNBT compoundNBT = (CompoundNBT)inbt;
+				if( compoundNBT.contains( registryNameName, Constants.NBT.TAG_STRING ) ) {
+					String registry_name_string = compoundNBT.getString( registryNameName );
+					if( ResourceLocation.isResouceNameValid( registry_name_string ) &&
+						compoundNBT.contains( xName, Constants.NBT.TAG_INT ) &&
+						compoundNBT.contains( yName, Constants.NBT.TAG_INT ) &&
+						compoundNBT.contains( zName, Constants.NBT.TAG_INT ) ) {
+						ResourceLocation registry_name = new ResourceLocation( registry_name_string );
+						BlockPos pos = new BlockPos( new BlockPos( compoundNBT.getInt( xName ),
+							compoundNBT.getInt( yName ), compoundNBT.getInt( zName ) ) );
+						ISpawnBlockFactory factory = SPAWN_BLOCKING_REGISTERY.get( registry_name );
+						if( factory != null ) {
+							spawnBlockers.add( factory.buildSpawnBlocker( pos ) );
+						}
+					}
+				}
+			}
+		}
+		return spawnBlockers;
+	}
+}
