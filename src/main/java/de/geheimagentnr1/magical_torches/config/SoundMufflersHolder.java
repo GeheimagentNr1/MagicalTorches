c1dc2e2162ee2756296d2d0216af119ea6a3978d
GeheimagentNr1 <geheimelache@hotmail.com>
2021-05-01 20:37:28 +0200

    Changes for 1.16.1

--- src/main/java/de/geheimagentnr1/magical_torches/config/SoundMufflersHolder.java
+++ src/main/java/de/geheimagentnr1/magical_torches/config/SoundMufflersHolder.java
@@ -2,7 +2,8 @@ package de.geheimagentnr1.magical_torches.config;
 
 import de.geheimagentnr1.magical_torches.elements.capabilities.sound_muffling.SoundMuffler;
 import de.geheimagentnr1.magical_torches.helpers.SoundMufflerHelper;
-import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.World;
 
 import java.util.Optional;
 import java.util.TreeMap;
@@ -12,20 +13,21 @@ import java.util.TreeSet;
 public class SoundMufflersHolder {
 	
 	
-	private static TreeMap<DimensionType, TreeSet<SoundMuffler>> DIMENSION_SOUND_MUFFLERS =
+	private static TreeMap<RegistryKey<World>, TreeSet<SoundMuffler>> DIMENSION_SOUND_MUFFLERS =
 		SoundMufflerHelper.buildDimensionSoundMufflersTreeMap();
 	
-	public static Optional<TreeSet<SoundMuffler>> getDimensionSoundMufflers( DimensionType dimension ) {
+	public static Optional<TreeSet<SoundMuffler>> getDimensionSoundMufflers( RegistryKey<World> dimension ) {
 		
 		return Optional.ofNullable( DIMENSION_SOUND_MUFFLERS.get( dimension ) );
 	}
 	
-	public static TreeMap<DimensionType, TreeSet<SoundMuffler>> getDimensionSoundMufflers() {
+	public static TreeMap<RegistryKey<World>, TreeSet<SoundMuffler>> getDimensionSoundMufflers() {
 		
 		return DIMENSION_SOUND_MUFFLERS;
 	}
 	
-	public static void setDimensionSoundMufflers( TreeMap<DimensionType, TreeSet<SoundMuffler>> _dimensionSoundMufflers ) {
+	public static void setDimensionSoundMufflers(
+		TreeMap<RegistryKey<World>, TreeSet<SoundMuffler>> _dimensionSoundMufflers ) {
 		
 		DIMENSION_SOUND_MUFFLERS = _dimensionSoundMufflers;
 	}
